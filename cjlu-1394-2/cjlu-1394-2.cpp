4bfdb021112b1b684f0ed447339a586d4073afe6
Aminor-z <1079465529@qq.com>
2020-03-17 15:04:27 +0800

    upload

--- cjlu-1394-2/cjlu-1394-2.cpp
+++ cjlu-1394-2/cjlu-1394-2.cpp
@@ -1,52 +1,63 @@
 ﻿#pragma warning(disable:4996)
-#include <iostream>
-#include <math.h>
-#include <vector>
 #include <algorithm>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
 #include <string.h>
+#include <sstream>
+#include <vector>
 using namespace std;
-short fsg[10001];
-short n;
-short vl;
-short v[1000];
-inline bool fakesg(int num) {
-	if (fsg[num] != -1) {
-		return fsg[num];
-	}
-	bool flag = true;
-	for (int i=0; i<vl; i++)
-	{
-		if (n - v[i] < 0)return false;
-		if (!fakesg(num-v[i])) {
-			fsg[num] = true;
-			flag = false;
-		}
-	}
-	if(flag)
-		fsg[num] = false;
-	return fsg[num];
-
-}
-
+const int limit = 1e5 + 10;
+int DP[limit] = {0,};
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(0);
+	int n,t;
 	while (cin >> n) {
-		memset(fsg, -1, sizeof(fsg));
-		short x,i=0;
-		cin >> x;
-		for (; i < x; i++)
+		set<int> _set;
+		int num;
+		cin >> num;
+		for (int i = 0; i < num; i++)
 		{
-			short t;
 			cin >> t;
-			v[i] = t;
-			fsg[t] = true;
-			fsg[t<<1] = false;
+			_set.insert(t);
 		}
-		vl = x;
-		if (fakesg(n))cout << "Vector";
-		else cout << "Jenny";
-		cout << endl;
+		set<int> cb;
+		for (set<int>::iterator a = _set.begin(); a != _set.end(); a++)
+		{
+			DP[*a] = 1;
+			for (set<int>::iterator b = _set.end(); b != _set.end(); b++)
+			{
+				cb.insert(*a ** b);
+			}
+		}
+		int t = 1;
+		for (int i = 0; i < n; i++,t++)
+		{
+			for (set<int>::iterator a = _set.begin(); a != _set.end(); a++)
+			{
+				if (t & 1) {
+					int pos = i + *a;
+					if (DP[pos] == 1 || DP[pos] == -1)continue;
+					else DP[pos] = 1;
+				}
+				else
+				{
+					int pos = i + *a;
+					if(DP[pos]==0)
+					DP[pos] = -1;
+				}
+			}
+		}
+		if (DP[n] == 1)
+		{
+			cout << "Victor" << endl;
+		}
+		else cout << "Jenny" << endl;
+		
 	}
 
 
---
+++ cjlu-3337/cjlu-3337.cpp
@@ -0,0 +1,50 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	int amount;
+	cin >> amount;
+	for (int i = 0; i < amount; i++)
+	{
+		int set = 0, a = 0, b = 0, input = 0;
+		cin >> set;
+		for (int c = 0; c < set; c++)
+		{
+			cin >> input;
+			switch (input)
+			{
+			case 1:
+				a++;
+				break;
+			case 2:
+				b++;
+				break;
+			case 3:
+				a++; b++; break;
+			case 4:
+				a--; b--; break;
+			}
+		}
+
+		if (a > b)
+			cout << "Kobayashi" << endl;
+		if (a == b)
+			cout << "Draw" << endl;
+		if (a < b)
+			cout << "Tohru" << endl;
+	}
+	return 0;
+}
+
