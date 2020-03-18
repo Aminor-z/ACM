00b0a6ddc63b91a53b8353f85caeec8944629845
Aminor-z <1079465529@qq.com>
2020-03-18 20:41:46 +0800

    upload

---
+++ Arab Collegiate Programming Contest 2015 D Fractionstellar/Arab Collegiate Programming Contest 2015 D Fractionstellar.cpp
@@ -0,0 +1,65 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+struct fs
+{
+	unsigned long long fz, fm;
+};
+inline unsigned long long gcd(const unsigned long long& a, const unsigned long long& b)
+{
+	return b > 0 ? gcd(b, a % b) : a;
+}
+inline unsigned long long lcm(unsigned long long a, unsigned long long b) {
+	return a * b / gcd(a, b);
+}
+inline fs gcd(const fs& a, const fs& b)
+{
+	fs t;
+	t.fz = gcd(a.fz, b.fz);
+	t.fm = lcm(a.fm, b.fm);
+	return t;
+}
+inline fs lcm(const fs& a, const fs& b)
+{
+	fs t;
+	t.fz = lcm(a.fz, b.fz);
+	t.fm = gcd(a.fm, b.fm);
+	return t;
+}
+inline void simplify(fs& a)
+{
+	unsigned long long _gcd = gcd(a.fz, a.fm);
+	if (_gcd > 1)
+	{
+		a.fz /= _gcd;
+		a.fm /= _gcd;
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	register fs a, b, _gcd, _lcm;
+	for (int w = 0; w < n; w++)
+	{
+		cin >> a.fz >> a.fm >> b.fz >> b.fm;
+		simplify(a);
+		simplify(b);
+		_gcd = gcd(a, b);
+		_lcm = lcm(a, b);
+		simplify(_gcd);
+		simplify(_lcm);
+		cout << _gcd.fz << "/" << _gcd.fm << " " << _lcm.fz << "/" << _lcm.fm << endl;
+
+	}
+	return 0;
+}
---
+++ Arab Collegiate Programming Contest 2015 J Banana/Arab Collegiate Programming Contest 2015 J Banana.cpp
@@ -0,0 +1,50 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	//std::ios::sync_with_stdio(false);
+	//std::cin.tie(0);
+	int n;
+	cin >> n;
+	map<string, string> m;
+	string s, t;
+	for (register int i = 0; i < n; i++)
+	{
+		cin >> s >> t >> t;
+		m[s] = t;
+	}
+	cin >> n;
+	for (register int w = 0; w < n; w++)
+	{
+		int num;
+		cin >> num;
+		vector<string> v;
+		for (int i = 0; i < num; i++)
+		{
+			cin >> t;
+			v.push_back(t);
+		}
+		for (string& s : v)
+		{
+			s = m[s];
+		}
+		for (vector<string>::iterator iter = v.begin(); iter != v.end() - 1; iter++)
+		{
+			cout << *iter << " ";
+		}
+		cout << *(v.end() - 1) << endl;
+	}
+	return 0;
+}
+
