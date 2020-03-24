09d585aa8b7b423d03e65c1f9475673548d40283
Aminor-z <1079465529@qq.com>
2020-03-24 22:34:06 +0800

    upload

--- Temp/Temp.cpp
+++ Temp/Temp.cpp
@@ -1,5 +1,4 @@
 ﻿#pragma warning(disable:4996)
-#include <b>
 #include <algorithm>
 #include <functional>
 #include <map>
@@ -12,93 +11,20 @@
 #include <string.h>
 #include <sstream>
 #include <vector>
+#include <bitset>
 using namespace std;
 
-struct Node
-{
-	Node() {}
-	Node(bool isGreat) { great = isGreat; }
-	vector<int> next;
-	double value = -1;
-	bool great = false;
-};
-vector<Node> v;
-int N;
-double Z, R;
-double ans = 0.0;
-double dfs_1(const int& source, double value)
-{
-	double _ans = 0.0;
-	if (v[source].great)
-	{
-		//cout << "Great:" << v[source].value * value << endl;
-		return v[source].value * value;
-	}
-	else
-	{
-		for (int t : v[source].next)
-		{
-			//cout << source << "--->" << t << endl;
-			_ans += dfs_1(t, value * R);
-		}
-	}
-	return _ans;
-}
-void dfs_2(const int& source, double value)
-{
-
-	if (v[source].great)
-	{
-		//cout << "Great:" << v[source].value * value << endl;
-		ans += v[source].value * value + 1e-7;
-	}
-	else
-	{
-		for (int t : v[source].next)
-		{
-			//cout << source << "--->" << t << endl;
-			dfs_2(t, value * R);
-		}
-	}
-}
 int main()
 {
-	//std::ios::sync_with_stdio(false);
-	//std::cin.tie(0);
-	while (cin >> N >> Z >> R)
-	{
-		R = 1 - R * 0.01;
-		v.clear();
-		v.resize(N);
-		register unsigned long long n, t;
-		for (register int w = 0; w < N; w++)
-		{
-			cin >> n;
-			v[w] = Node();
-			if (n > 0)
-			{
-				for (register int i = 0; i < n; i++)
-				{
-					cin >> t;
-					v[w].next.push_back(t);
-				}
-			}
-			else
-			{
-				v[w] = Node(true);
-				cin >> t;
-				v[w].value = t;
-			}
-
-		}
-		if (N < 100000)
-			cout << (long long)dfs_1(0, Z) << endl;
-		else {
-			dfs_2(0, Z);
-			cout << (long long)(ans + 1e-5) << endl;
-		}
-	}
-
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	bitset<16> a;
+	register int t,flag=0;
+	cin >> t;
+	a = t;
+	unsigned long long w=a.to_ullong();
+	printf("%o\n", w);
+	printf("%X\n", w);
 	return 0;
 }
 
---
+++ cjlu-2803/cjlu-2803.cpp
@@ -0,0 +1,78 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+bool isEnemy[200][200];
+//并查集
+int DSU[200] = { 0 };
+inline int find(const int& x) {
+	int t = x;
+	while (1) {
+		if (DSU[t] == 0) {
+			return t;
+		}
+		t = DSU[t];
+	}
+	//	return r;
+}
+//不合并
+//A
+//|——|
+//x  y
+//合并
+//x y ---------> x-y
+inline void add(const int& n, const int& m) {
+	int x = find(n);
+	int y = find(m);
+	if (x != y)
+		DSU[x] = y;
+}
+int main() {
+	register int n, m, k, a, b, _isEnemy;
+	while (cin >> n >> m >> k)
+	{
+		memset(DSU, 0, sizeof(DSU));
+		for (register int i = 0; i < 200; i++)
+		{
+			memset(isEnemy[i], false, sizeof(isEnemy[i]));
+		}
+		for (register int i = 0; i < m; i++) {
+			cin >> a >> b >> _isEnemy;
+			if (_isEnemy == 1) {
+				add(a, b);
+			}
+			else {
+				isEnemy[a][b] = true;
+				isEnemy[b][a] = true;
+			}
+		}
+		for (register int i = 0; i < k; i++) {
+			cin >> a >> b;
+			bool isSame = find(a) == find(b);
+			if (isSame && !isEnemy[a][b]) {
+				cout << "No problem\n";
+			}
+			else if (!isSame && !isEnemy[a][b]) {
+				cout << "OK\n";
+			}
+			else if (isSame && isEnemy[a][b]) {
+				cout << "OK but..." << endl;
+			}
+
+			else if (isEnemy[a][b] && !isSame) {
+				cout << "No way\n";
+			}
+		}
+	}
+	return 0;
+}
