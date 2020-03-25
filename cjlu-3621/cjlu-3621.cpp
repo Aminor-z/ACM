8cea3390567d371a4f263421d4c0737fe1419918
Aminor-z <1079465529@qq.com>
2020-03-25 14:06:15 +0800

    Create cjlu-3621.cpp

---
+++ cjlu-3621/cjlu-3621.cpp
@@ -0,0 +1,23 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	return 0;
+}
+
