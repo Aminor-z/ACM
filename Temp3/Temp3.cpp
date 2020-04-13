b4080936b1e3914aa3ac6e5f976c401b9b78a9d1
Aminor-z <1079465529@qq.com>
2020-04-13 15:32:51 +0800

    upload

--- Temp3/Temp3.cpp
+++ Temp3/Temp3.cpp
@@ -12,21 +12,58 @@
 #include <sstream>
 #include <vector>
 using namespace std;
-string decode = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
-int main()
-{
-	//std::ios::sync_with_stdio(false);
-	//std::cin.tie(0);
-    register int i, c;
-    while ((c = getchar()) != EOF) {
-        for (i = 1; decode[i] && decode[i] != c; ++i);
-        {
-            if (c == '\n')continue;
-            if (decode[i])
-                putchar(decode[i - 1]);
-            else putchar(c);
+struct projection {
+    int x, y;
+    projection() {}
+    projection(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+};
+vector<int> y[201];
+vector<int> z[201];
+bool check(int n) {
+    for (int i = 0; i < n; ++i) {
+        if (y[i].size() > 0 && z[i].size() == 0) return false;
+        if (y[i].size() == 0 && z[i].size() > 0) return false;
+    }
+    return true;
+}
+int main() {
+    int n, m, h;
+    scanf("%d%d%d", &n, &m, &h);
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j) {
+            char ch;
+            scanf(" %c", &ch);
+            if (ch == '1') y[i].push_back(j);
+        }
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < h; ++j) {
+            char ch;
+            scanf(" %c", &ch);
+            if (ch == '1') z[i].push_back(j);
+        }
+    if (!check(n)) {
+        puts("-1");
+        return 0;
+    }
+    int ans = 0;
+    for (int x = 0; x < n; ++x) ans += y[x].size() * z[x].size();
+    printf("%d\n", ans);
+    for (int x = 0; x < n; ++x) {
+        for (auto Y : y[x])
+            for (auto Z : z[x]) {
+                printf("%d %d %d\n", x, Y, Z);
+            }
+    }
+    ans = 0;
+    for (int x = 0; x < n; ++x) ans += max(y[x].size(), z[x].size());
+    printf("%d\n", ans);
+    for (int x = 0; x < n; ++x) {
+        int len = max(y[x].size(), z[x].size());
+        for (int i = len; i >= 1; --i) {
+            printf("%d %d %d\n", x, y[x][max(0ull, y[x].size() - i)], z[x][max(0ull, z[x].size() - i)]);
         }
     }
-	return 0;
 }
-
---
+++ cjlu-3336/cjlu-3336.cpp
@@ -0,0 +1,80 @@
+﻿#pragma warning(disable:4996)
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string.h>
+#include <sstream>
+#include <vector>
+#include <bitset>
+using namespace std;
+const int LIMIT = 1500 << 3;
+inline string summon(const int& n,const int&num)
+{
+	switch (n%100)
+	{
+	case 11:
+		return "The " + to_string(n) + "th ugly number is " + to_string(num) + ".\n";
+	case 12:
+		return "The " + to_string(n) + "th ugly number is " + to_string(num) + ".\n";
+	case 13:
+		return "The " + to_string(n) + "th ugly number is " + to_string(num) + ".\n";
+	default:
+		switch (n % 10)
+		{
+		case 1:
+			return "The " + to_string(n) + "st ugly number is " + to_string(num) + ".\n";
+		case 2:
+			return "The " + to_string(n) + "nd ugly number is " + to_string(num) + ".\n";
+		case 3:
+			return "The " + to_string(n) + "rd ugly number is " + to_string(num) + ".\n";
+		default:
+			return "The " + to_string(n) + "th ugly number is " + to_string(num) + ".\n";
+		}
+	}
+	
+}
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+	register long long a, b, c;
+	priority_queue<long long, vector<long long>, greater<long long> > q;
+	vector<long long> v;
+	v.push_back(0);
+	v.push_back(1);
+	q.push(1);
+	while (v.size() < LIMIT)
+	{
+		a = q.top() << 1;
+		b = a + q.top();
+		c = a + b;
+		q.pop();
+		if (a % 5 != 0 && a % 3 != 0)
+		{
+			q.push(a);
+			v.push_back(a);
+		}
+		if (b % 5 != 0)
+		{
+			q.push(b);
+			v.push_back(b);
+		}
+		q.push(c);
+		v.push_back(c);
+
+	}
+	sort(v.begin(), v.end());
+	register int op = 0;
+	while (cin >> op && op != -1)
+	{
+		cout << summon(op, v[op]);
+	}
+	return 0;
+}
+
